4994ed449ef37e4dc75494cb9c53b2e9070f8e3a
gkibria <gkibria121@gmail.com>
2024-11-29 23:01:31 +0600

    schedule and reschedule a task

---
+++ manager.py
@@ -0,0 +1,17 @@
+import datetime
+from autogen import ConversableAgent, AssistantAgent, UserProxyAgent, initiate_chats
+from autogen.coding import LocalCommandLineCodeExecutor
+from pprint import pprint
+from task_scheduler import schedule_task
+ 
+if __name__=="__main__":
+    print("Choose your workflow.")
+    print("1. Schedule a task")
+    print("2. Reschedule a task")
+    print("3. Complete a task")
+
+    choise = input("Enter your choise: ")
+    if(choise=="1"):
+        initiate_chats(schedule_task())
+    if(choise=="2"):
+        initiate_chats(schedule_task())
--- task_reschedule.py
+++ task_reschedule.py
@@ -3,7 +3,7 @@ from autogen import ConversableAgent, AssistantAgent, UserProxyAgent, initiate_c
 from autogen.coding import LocalCommandLineCodeExecutor
 from pprint import pprint
 # Utility function to be called
-def add_task_reminder(start, end):
+def update_task_reminder(start, end):
     """
     Print task details.
 
@@ -17,6 +17,16 @@ def add_task_reminder(start, end):
     print(f"Task Start: {start}")
     print(f"Task End: {end}") 
 
+def remove_task_reminder():
+    """
+    Print task details.
+
+    Parameters:
+        no parameters
+    Returns:
+    None
+    """ 
+    print("task is completed")
 
 # LLM Configuration
 llm_config = {
@@ -30,7 +40,7 @@ llm_config = {
 executor = LocalCommandLineCodeExecutor(
     timeout=200,
     work_dir="coding",
-    functions=[add_task_reminder]
+    functions=[update_task_reminder,remove_task_reminder]
 )
 
 
@@ -39,12 +49,16 @@ current_time = datetime.datetime.now()
 # Time Allocation Agent
 time_allocation_agent = AssistantAgent(
             name="time_allocation_agent", 
-            system_message="You are a Time Allocation Agent. "
+            system_message="You are a Time Re-Allocation Agent. "
                         f"The current system time is {current_time.strftime('%Y-%m-%d %H:%M:%S')}. " 
-                        "Ask the user about task start time and duration. "
-                        "When details are collected, prepare to pass them to the processor." 
-                            "After getting avobe information repeat it and add"
-                            "Please answer with 'TERMINATE' if I have correctly your description",
+                        "Ask the user whether  task is done or not. "
+                        "If he has done the task , do not ask him when he can start and duration"
+                        "If he did not done the task "
+                         "Ask the user when can  start task  and duration. "
+                        "After getting avobe information repeat it and add"
+                        "Please answer with 'TERMINATE' if I have correctly your description" 
+                        ,
+                        
             llm_config=llm_config,
             human_input_mode="NEVER",
             is_termination_msg=lambda msg: "terminate" in msg.get("content").lower(),
@@ -83,17 +97,25 @@ chats = [
     {
         "sender": time_allocation_agent,
         "recipient": user_proxy_agent,
-        "message": "When can you start the task and how long will it take?",
+        "message": "Have you finished your task?",
         "summary_method": "reflection_with_llm",
         "summary_args": {
-            "summary_prompt": "Extract task timing in JSON: "
-                              "{'start': 'YYYY-MM-DD HH:mm', 'end' : 'YYYY-MM-DD HH:mm'   }"
+            "summary_prompt": "If user did complete his task"
+            "Give me the JSON"
+            "{  status : 'completed'  }"
+            "If user did not complete the task"
+            "Extract task timing in JSON: "
+                              "{'start': 'YYYY-MM-DD HH:mm', 'end' : 'YYYY-MM-DD HH:mm' , status : 'incomplete'  }"
         }
     },
     {
         "sender": time_allocation_agent,
         "recipient": code_processor_agent, 
-        "message": "Import the add_task_reminder_function from functions, call the function with parameter start and end time",
+        "message": """ 
+        If the status of JSON object is incomplete , you will write a python block importing update_task_reminder from functions and call it with start and end time
+        If the status of JSON object is completed , you will write a python block importing remove_task 
+
+        """,
         "summary_method": "reflection_with_llm", 
         "summary_args": {
             "summary_prompt": "Only python block  , without any comment or explain "
@@ -111,6 +133,6 @@ chats = [
 ]
 
 
-def schedule_task():
-   return chats
+ 
 
+initiate_chats(chats)
